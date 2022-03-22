8227fcf8d4080cba9392c26453748e7b85b8e874
Вадим <nerzhularthas@gmail.com>
2022-03-22 23:59:46 +0600

    Big map variant

--- vxs-triange-app/src/App.tsx
+++ vxs-triange-app/src/App.tsx
@@ -9,7 +9,7 @@ enum TriangleType {
 }
 
 function drawPoly(
-  coords: string,
+  coords: number[],
   scol: string | CanvasGradient | CanvasPattern = "#808080",
   fcol: string | CanvasGradient | CanvasPattern = "#FFFFFF"
 ) {
@@ -20,19 +20,17 @@ function drawPoly(
 
   if (null == ctx) return "";
 
-  let points = coords.split(',');
-
   ctx.beginPath();
   ctx.strokeStyle = scol;
   ctx.fillStyle = fcol;
 
-  ctx.moveTo(+points[0], +points[1]); // `
+  ctx.moveTo(coords[0], coords[1]); // `
 
-  for (let i = 1; i < points.length / 2; i++) {
-    ctx.lineTo(+points[i * 2], +points[i * 2 + 1]);
+  for (let i = 1; i < coords.length / 2; i++) {
+    ctx.lineTo(coords[i * 2], coords[i * 2 + 1]);
   }
 
-  ctx.lineTo(+points[0], +points[1]);
+  ctx.lineTo(coords[0], coords[1]);
 
   ctx.fill();
   ctx.stroke();
@@ -42,66 +40,83 @@ function drawPoly(
 }
 
 interface TriangleProps {
-  id: any,
   type: TriangleType,
   scol?: string | CanvasGradient | CanvasPattern,
   fcol?: string | CanvasGradient | CanvasPattern,
-  onClick?: (...args: any[]) => void
+}
+
+function makeTriangeCoords(type: TriangleType, width: number, height: number) {
+  switch (type) {
+    case TriangleType.IsoscelesDown:
+      return [0, 0, width / 2, height, width, 0];
+    case TriangleType.IsoscelesUp:
+      return [0, height, width / 2, 0, width, height];
+  }
+  return [0, 0];
 }
 
 function Triangle(props: TriangleProps) {
   const type = props.type;
   const scol = undefined === props.scol ? "#808080" : props.scol;
   const fcol = undefined === props.fcol ? "#FFFFFF" : props.fcol;
-
-  const id = props.id;
-  const mapName = `#${id}`;
-
-  function onClick(ev: any) {
-    if (undefined === props.onClick) return;
-    props.onClick(props.id);
-  }
+  const mapName = props.useMap;
 
   let altText = "";
-  let areaCoords = "";
+  const areaCoords = makeTriangeCoords(type, 50, 30);
   switch (type) {
     case TriangleType.IsoscelesDown:
       altText = '\\/';
-      areaCoords = "0,0,25,30,50,0";
       break;
     case TriangleType.IsoscelesUp:
       altText = '/\\';
-      areaCoords = "0,30,25,0,50,30";
       break;
   }
 
   const dataUrl = drawPoly(areaCoords, scol, fcol);
 
   return (
-    <>
-      <map name={id}>
-        <area shape="poly" coords={areaCoords}
-          onClick={onClick} href="#" alt="" />
-      </map>
-      <img src={dataUrl} alt={altText} style={{ marginRight: "-25px" }} useMap={mapName} />
-    </>
+    <img src={dataUrl} alt={altText} style={{ marginRight: "-25px" }} useMap={mapName} />
   )
 }
 
 function App() {
+  let engine = newEngine();
 
-  function onclick(ev: any) {
-    console.log(ev);
+  engine.GenerateFiled(0)
+
+  function onClick(ev: any, x: number, y: number) {
+    console.log(ev, x, y);
+  }
+
+  function make() {
+    let w = 50;
+    let h = 30;
+    let res = [];
+    for (let i = 0; i < engine.Width + 2; i++) {
+      for (let j = 0; j < engine.Heigh + 4; j++) {
+        let type = (j % 2 + (i + 1) % 2) % 2 === 0 ? TriangleType.IsoscelesDown : TriangleType.IsoscelesUp;
+        // let ch = engine.GetTraingle(i, j);
+        let areaCoords = makeTriangeCoords(type, w, h).map((v, k) => k % 2 === 0 ? v + h * j : v + w * i);
+        res.push((
+          <area shape="poly" coords={areaCoords.join(",")} onClick={ev=>onClick(ev, j, i)} href="#" alt="" />
+        ));
+      }
+    }
+
+    return res;
   }
 
   return (
     <div>
-      {/* <img width={100} height={30}/> */}
+      <map name="map">
+        {make()}
+      </map>
       <div>
-        <Triangle id={0} type={TriangleType.IsoscelesUp} onClick={onclick} />
-        <Triangle id={1} type={TriangleType.IsoscelesDown} onClick={onclick} />
-        <Triangle id={2} type={TriangleType.IsoscelesUp} onClick={onclick} />
+        <Triangle type={TriangleType.IsoscelesUp} />
+        <Triangle type={TriangleType.IsoscelesDown} />
+        <Triangle type={TriangleType.IsoscelesUp} />
       </div>
+      <img width={(engine.Width + 2) * 50} height={(engine.Heigh + 2) * 30} useMap="#map"/>
     </div>
   );
 }
